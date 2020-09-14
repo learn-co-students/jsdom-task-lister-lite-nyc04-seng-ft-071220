ab7e53cbc931aa6aeb109bcd205ba7e94fc38712
David Felix <61062296+Dfelix02@users.noreply.github.com>
2020-09-14 15:07:26 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const myTodos = document.getElementById("tasks");
+  const form = document.getElementById("create-task-form");
+  const newListItem = document.createElement('li'); 
+  form.addEventListener("click", function(event){
+    let taskDescription = document.getElementById("new-task-description").value;
+    newListItem.textContent = taskDescription
+    myTodos.appendChild(newListItem);
+    event.preventDefault()
+
+  });
 });

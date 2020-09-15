acc9bedb9a90c4e799edd7f22ef4e2bb007b5aa7
wilsonng21 <wilsongch92@gmail.com>
2020-09-15 16:38:07 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  let taskForm = document.querySelector('#create-task-form')
+  let taskList = document.querySelector('#tasks')
+
+  taskForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+    let userInput = event.target['new-task-description'].value 
+    
+    addUserInputToTaskList(userInput)
+  })
+
+  function addUserInputToTaskList(userInput) {
+    let blankLi = document.createElement('li')
+    blankLi.innerText = userInput
+    taskList.append(blankLi)
+
+    blankLi.addEventListener("click", function(e){
+      // blankLi is accessible here because of closure
+      blankLi.remove()
+  })
+  }
+
+})

ba36f01e3645983001d4658a2412f579cf69ae91
Elronia <zar.catherine@gmail.com>
2020-09-15 17:53:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-    // initialize taskList class
-    const taskList = new TaskList();
-    //grab all the necessary DOM elements
-    //form and relevant input fields
-    const newTaskForm = document.getElementById("create-task-form");
-    const newTaskDescription = document.getElementById("new-task-description");
-    const newTaskPriority = document.getElementById("new-task-priority");
-    //ul where new tasks will live on the DOM
-    const taskUl = document.getElementById("tasks");
-    const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
-    //attach event listeners
-    newTaskForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      taskList.createNewTask(newTaskDescription.value);
-      // reset form
-      e.target.reset();
-      renderApp();
-    });
-    taskUl.addEventListener("click", (e) => {
-      if (e.target.nodeName === "BUTTON") {
-        taskList.deleteTask(e.target.dataset.description);
-        renderApp();
-      }
-    });
 });
+// your code here
+   const toDoList = document.querySelector("ul#tasks");
+   const createTaskForm = document.querySelector("form#create-task-form");
+
+   createTaskForm.addEventListener("submit", function(evt){
+     evt.preventDefault(); //not to trigger the action
+     const inputFormElement = evt.target;
+     let userInput = inputFormElement["new-task-description"].value;
+
+     let blankLi = document.createElement("li");
+     blankLi.innerText = userInput;
+     toDoList.append(blankLi);
+
+     let deleteButton = document.createElement("button");
+     deleteButton.setAttribute("data-description", userInput);
+     deleteButton.innerText = "X";
+     blankLi.append(deleteButton);
+
+     deleteButton.addEventListener("click", function() {
+       blankLi.remove();
+     })
+   })
+
 

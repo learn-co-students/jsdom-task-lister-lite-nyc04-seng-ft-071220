58dd396a3b5051cacd19c4dab5b46b249d5b3976
Chenyun Zhang <morningchenyun@gmail.com>
2020-09-15 13:46:31 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // console.log("loaded!");
 });
+
+
+let clickFormButton = document.querySelector("#create-task-form");
+let todoList = document.querySelector("ul#tasks");
+
+
+clickFormButton.addEventListener('submit', todoEvent = (e) => {
+  e.preventDefault()
+  let theFormElement = e.target;
+  let userInput = theFormElement["new-task-description"].value;
+  console.log(userInput);
+
+  let newList = document.createElement("li");
+  newList.innerText = userInput;
+  todoList.append(newList);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.setAttribute('data-description',userInput);
+  deleteButton.innerText="X";
+  newList.append(deleteButton);
+
+  
+  deleteButton.addEventListener("click", removeList = (e) =>{
+    newList.remove(deleteButton);
+  })
+})

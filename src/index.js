57e84f6b2c7ffea1d7c66d211f49fa8fdffcb456
sbalgobin94 <sbalgobin94@gmail.com>
2020-09-20 18:48:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let newTaskForm = document.querySelector("#create-task-form")
+  let tasksUL = document.querySelector("ul#tasks")
+
+
+  newTaskForm.addEventListener("submit", function(evt){
+    evt.preventDefault()
+    let whatUserTyped = evt.target["new-task-description"].value
+    
+
+    let taskLi = document.createElement("li")
+    taskLi.innerText = whatUserTyped
+    tasksUL.append(taskLi)
+
+    let btn = document.createElement("Button")
+    btn.innerText = "X"
+    taskLi.appendChild(btn)
+
+    btn.addEventListener("click", function(evt){
+      taskLi.remove()
+    })
+
+
+  })
+
 });
